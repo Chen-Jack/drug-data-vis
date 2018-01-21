867c1134ccc4bf2125339a7d81a187393ab68ff1
Jack C <chen.jack@email.com>
2018-01-21 16:42:44 -0500

    first iteration of csv retrieval completed

--- data_vis/data_vis/urls.py
+++ data_vis/data_vis/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from visual.views import HomeView
+from visual.views import HomeView, chemical_side_effects
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name = "home"),
+    url(r'^api/side_effect_data/$', chemical_side_effects, name="chemical_side_effects" )
 ]
--- data_vis/visual/views.py
+++ data_vis/visual/views.py
@@ -3,7 +3,55 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+# def chemical_side_effects(request):
+#     data = {}
+#     csv_data = open('/Users/jack/Desktop/chem_se_SIDER.csv', 'r')
+#     total_row = 0
+#     for line in csv_data:
+#         total_row += 1
+#     total_col = len(line)
+#     csv_data.close()
+
+#     csv_data = open('/Users/jack/Desktop/chem_se_SIDER.csv', 'r') 
+#     for row in range(total_col):
+#         line = csv_data.readline()
+#         for col in range(total_col):
+#             if(line.split(',')[col] == '1'):
+#                 data.update({row-1,col-1}) 
+
+#     csv_data.close()
+#     return JsonResponse(data)
+
+def chemical_side_effects(request):
+
+    #Just getting the dimensions of your matrix
+    csv_data = open('/Users/jack/Desktop/test.csv', 'r')
+    total_row = 0
+    for line in csv_data:
+        total_row += 1
+    total_col = len(line.split(','))
+    csv_data.close()
+
+    #Creating coordinates from csv file
+    counter = 0
+    coordinates = {} #A mapping of (chemicals , side effects)
+    csv_data = open('/Users/jack/Desktop/test.csv', 'r') 
+    for row in range(total_row):
+        line = csv_data.readline()
+        parsed_line = line.rstrip('\n').split(',')
+        for col in range(total_col):
+            if(parsed_line[col] == '1'):
+                coordinates.update( { str(counter) : (str(row-1) , str(col-1)) } ) # -1 cause database starts at 0
+                counter +=1 
+
+    csv_data.close()
+    return JsonResponse(coordinates)
+   
+       
+    
