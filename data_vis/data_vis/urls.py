554ada590740d17ecc98194d862a3b9c12e69201
Jack C <chen.jack@email.com>
2018-01-22 15:48:24 -0500

    new api endpoint for chemical list and cytoscape now only renders chemicals at init

--- data_vis/data_vis/urls.py
+++ data_vis/data_vis/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-from visual.views import HomeView, chemical_side_effects
+from visual.views import HomeView, chemical_side_effects, get_chemical_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name = "home"),
-    url(r'^api/side_effect_data/$', chemical_side_effects, name="chemical_side_effects" )
+    url(r'^api/side_effect_data/$', chemical_side_effects, name="chemical_side_effects" ),
+    url(r'^api/chemical_list/$', get_chemical_list, name="chemical_list"),
 ]
--- data_vis/visual/views.py
+++ data_vis/visual/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
+from .models import SideEffect, Chemical
+import json
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -12,20 +14,23 @@ class HomeView(TemplateView):
 # '/Users/jack/Desktop/chem_se_SIDER.csv'
 
 
+
+
 def chemical_side_effects(request):
 
     #Just getting the dimensions of your matrix
-    csv_data = open('/Users/jack/Desktop/test.csv', 'r')
+    csv_data = open('/Users/jack/Desktop/chem_se_SIDER.csv', 'r')
     total_row = 0
     for line in csv_data:
         total_row += 1
     total_col = len(line.split(','))
     csv_data.close()
+    
 
     #Creating coordinates from csv file
     counter = 0
     coordinates = {} #A mapping of (chemicals , side effects)
-    csv_data = open('/Users/jack/Desktop/test.csv', 'r') 
+    csv_data = open('/Users/jack/Desktop/chem_se_SIDER.csv', 'r') 
     for row in range(total_row):
         line = csv_data.readline()
         parsed_line = line.rstrip('\n').split(',')
@@ -37,5 +42,13 @@ def chemical_side_effects(request):
     csv_data.close()
     return JsonResponse(coordinates)
    
-       
-    
+def get_chemical_list(request, *args, **kwargs):
+    chemical_list = Chemical.objects.all()
+    qs = {}
+    counter = 0
+    for chemical in chemical_list:
+        qs.update({
+            str(counter): (chemical.CID, chemical.InChIKey, chemical.SMILES)
+        })
+        counter += 1
+    return JsonResponse(qs)
