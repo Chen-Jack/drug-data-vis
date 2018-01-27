fd7bf7c2445a229535785a6dcb32d9d42b35204b
Jack C <chen.jack@email.com>
2018-01-27 16:28:10 -0500

    clicking on a chemical now shows it's neighboring side effects and genes, I now need to apply the same feature for the other 2

--- data_vis/data_vis/urls.py
+++ data_vis/data_vis/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
-from visual.views import HomeView, get_chemical_list, get_related_side_effects
+from visual.views import HomeView, get_chemical_list, get_related_side_effects, get_related_genes
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name = "home"),
     url(r'^api/chemical_list/$', get_chemical_list, name="chemical_list"),
     url(r'^api/side_effects/(?P<CID>[0-9]*)/$', get_related_side_effects, name="side_effects"),
+    url(r'^api/genes/(?P<CID>[0-9]*)/$', get_related_genes, name="genes")
 ]
--- data_vis/visual/views.py
+++ data_vis/visual/views.py
@@ -11,28 +11,49 @@ class HomeView(TemplateView):
     template_name = "home.html"
 
 
-   
 def get_chemical_list(request, *args, **kwargs):
+    '''
+    API endpoint that returns all chemicals in the database.
+    Chemical: (CID, InChiKey, total_side_effects, total_genes)
+    '''
     chemical_list = Chemical.objects.all()
     qs = {}
     counter = 0
     for chemical in chemical_list:
-        total_effects = len(chemical.sideeffect_set.all())
+        total_side_effects = chemical.total_associated_side_effects
+        total_genes = chemical.total_associated_genes
         qs.update({
-            str(counter): (chemical.CID, chemical.InChIKey, total_effects)
+            str(counter): (chemical.CID, chemical.InChIKey, total_side_effects, total_genes )
         })
         counter += 1
     return JsonResponse(qs)
 
 def get_related_side_effects(request, *args, **kwargs):
+    '''
+    API endpoint that returns all side_Effect entries given some CID 
+    Side effect: (Name, UMLS_CUI)
+    '''
     CID_query = int(kwargs['CID']) 
     chem_obj = Chemical.objects.get(CID = CID_query)
-    se_list = chem_obj.sideeffect_set.all()
+    se_qs = chem_obj.associated_side_effects.all()
 
     data={}
     counter = 0
-    for se in se_list:
+    for se in se_qs:
         data.update({str(counter):(se.name, se.UMLS_CUI)})
         counter += 1
 
+    return JsonResponse(data)
+
+def get_related_genes(request, *args, **kwargs):
+    
+    CID_query = int(kwargs['CID'])
+    chem_obj = Chemical.objects.get(CID = CID_query)
+    gene_qs = chem_obj.associated_genes.all()
+
+    data = {}
+    counter = 0
+    for gene in gene_qs:
+        data.update({str(counter): (gene.HGNC, gene.Symbol, gene.UniProt, gene.Chromosome, gene.total_associated_side_effects, gene.total_associated_chemicals)})
+        counter += 1
     return JsonResponse(data)
