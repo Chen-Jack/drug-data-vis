dfcf2ba385a46c2b4a1ff6c691249d3391e7cc5f
Jack C <chen.jack@email.com>
2018-01-27 18:23:29 -0500

    added basic functionality for traversing through layers by clicking

--- data_vis/data_vis/urls.py
+++ data_vis/data_vis/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from visual.views import HomeView, get_chemical_list, get_related_side_effects, get_related_genes
+from visual.views import HomeView, get_chemical_list, get_related_side_effects, get_related_genes, get_related_chemicals
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name = "home"),
     url(r'^api/chemical_list/$', get_chemical_list, name="chemical_list"),
-    url(r'^api/side_effects/(?P<CID>[0-9]*)/$', get_related_side_effects, name="side_effects"),
-    url(r'^api/genes/(?P<CID>[0-9]*)/$', get_related_genes, name="genes")
+    url(r'^api/associated_side_effects/(?P<type>\w+)/(?P<ID>\w+)/$', get_related_side_effects, name="side_effects"),
+    url(r'^api/associated_genes/(?P<type>\w+)/(?P<ID>\w+)/$', get_related_genes, name="genes"),
+    url(r'^api/associated_chemicals/(?P<type>\w+)/(?P<ID>\w+)/$', get_related_chemicals, name="chemicals")
 ]
--- data_vis/visual/views.py
+++ data_vis/visual/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse, HttpResponse
-from .models import SideEffect, Chemical
+from .models import SideEffect, Chemical, Gene
 
 
 class HomeView(TemplateView):
@@ -29,27 +29,37 @@ def get_chemical_list(request, *args, **kwargs):
     return JsonResponse(qs)
 
 def get_related_side_effects(request, *args, **kwargs):
-    '''
-    API endpoint that returns all side_Effect entries given some CID 
-    Side effect: (Name, UMLS_CUI)
-    '''
-    CID_query = int(kwargs['CID']) 
-    chem_obj = Chemical.objects.get(CID = CID_query)
-    se_qs = chem_obj.associated_side_effects.all()
+    se_qs = None
+
+    if(kwargs['type'] == 'chemical'):
+        CID_query = int(kwargs['ID']) 
+        chem_obj = Chemical.objects.get(CID = CID_query)
+        se_qs = chem_obj.associated_side_effects.all()
+
+    elif(kwargs['type'] == 'gene'):
+        HGNC_query = int(kwargs['ID'])
+        gene_obj = Gene.objects.get(HGNC = HGNC_query)
+        se_qs = gene_obj.associated_side_effects.all()
 
     data={}
     counter = 0
     for se in se_qs:
-        data.update({str(counter):(se.name, se.UMLS_CUI)})
+        data.update({str(counter):(se.name, se.UMLS_CUI, se.total_associated_chemicals, se.total_associated_genes)})
         counter += 1
 
     return JsonResponse(data)
 
+
 def get_related_genes(request, *args, **kwargs):
-    
-    CID_query = int(kwargs['CID'])
-    chem_obj = Chemical.objects.get(CID = CID_query)
-    gene_qs = chem_obj.associated_genes.all()
+    if(kwargs['type'] == 'chemical'):
+        CID_query = int(kwargs['ID'])
+        chem_obj = Chemical.objects.get(CID = CID_query)
+        gene_qs = chem_obj.associated_genes.all()
+
+    elif(kwargs['type'] == "side_effect"):
+        SE_Query = kwargs['ID']
+        se_obj = SideEffect.objects.get(UMLS_CUI = SE_Query)
+        gene_qs = se_obj.gene_set.all()
 
     data = {}
     counter = 0
@@ -57,3 +67,24 @@ def get_related_genes(request, *args, **kwargs):
         data.update({str(counter): (gene.HGNC, gene.Symbol, gene.UniProt, gene.Chromosome, gene.total_associated_side_effects, gene.total_associated_chemicals)})
         counter += 1
     return JsonResponse(data)
+
+def get_related_chemicals(request, *args, **kwargs):
+    if(kwargs['type'] == 'gene'):
+        HGNC_query = int(kwargs['ID'])
+        gene_obj = Gene.objects.get(HGNC = HGNC_query)
+        chem_qs = gene_obj.chemical_set.all()
+
+    elif(kwargs['type'] == "side_effect"):
+        print("yes, all chemicsals related to side effect chosen")
+        SE_Query = kwargs['ID']
+        se_obj = SideEffect.objects.get(UMLS_CUI = SE_Query)
+        chem_qs = se_obj.chemical_set.all()
+
+    print("length is " + str(len(chem_qs)))
+    data = {}
+    counter = 0
+    for chem in chem_qs:
+        data.update({str(counter):(chem.CID, chem.InChIKey, chem.total_associated_side_effects, chem.total_associated_genes)})
+        counter += 1
+    return JsonResponse(data)
+
