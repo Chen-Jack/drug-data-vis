87847db095cde703bfc5194fe811c3f21cb103e9
Jack C <chen.jack@email.com>
2018-02-03 19:20:09 -0500

    adding intermodel relations

--- data_vis/visual/admin.py
+++ data_vis/visual/admin.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Chemical, SideEffect, Gene
+from .models import Chemical, SideEffect, Gene, se_similarity,gene_similarity,chem_similarity
 
 # Register your models here.
 
 admin.site.register(SideEffect)
 admin.site.register(Chemical)
 admin.site.register(Gene)
+admin.site.register(se_similarity)
+admin.site.register(gene_similarity)
+admin.site.register(chem_similarity)
--- data_vis/visual/models.py
+++ data_vis/visual/models.py
@@ -83,10 +83,29 @@ class Chemical(models.Model):
             parsed_entry = entry.split('\t')
             Chemical.objects.create(CID = parsed_entry[1], InChIKey = parsed_entry[2], SMILES=parsed_entry[3])
         CHEM_data.close()
-        
-        
 
 
+#Intermediate Tables for intermodel similarities and interactions, not meant to be used standalone models
+class se_similarity(models.Model):
+    se_a = models.ForeignKey(SideEffect, related_name='se_a_set')
+    se_b = models.ForeignKey(SideEffect, related_name='se_b_set')
+    similarity = models.FloatField(null=True, blank=True)
+
+        
+class chem_similarity(models.Model):
+    chem_a= models.ForeignKey(Chemical, on_delete=models.CASCADE, related_name='chem_a_set')
+    chem_b = models.ForeignKey(Chemical, on_delete=models.CASCADE, related_name='chem_b_set')
+    similarity = models.FloatField(null=True, blank=True)
+
+class gene_similarity(models.Model):
+    gene_a = models.ForeignKey(Gene, related_name='gene_a_set')
+    gene_b = models.ForeignKey(Gene, related_name='gene_b_set')
+    similairty = models.FloatField(null=True, blank=True)
+    interaction = models.FloatField(null=True, blank=True)
+
+
+
+#Model Functions
 def initChemToSideEffectRelations():
     '''
     Establishes the Many-To-Many connection between Chemicals and Side Effects"
@@ -194,7 +213,6 @@ def initGeneToSideEffectRelations():
     for se in se_qs:
         se.total_associated_genes = len(se.gene_set.all())
         se.save()
-
 
 def clearRelations():
     gene_qs = Gene.objects.all()
