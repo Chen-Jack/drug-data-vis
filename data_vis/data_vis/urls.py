00043bec60873ac9d3013c8f21a917f6269c1ce8
Jack C <chen.jack@email.com>
2018-02-04 19:15:03 -0500

    updated urls to include single node insertion views

--- data_vis/data_vis/urls.py
+++ data_vis/data_vis/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^api/associated_genes/(?P<type>\w+)/(?P<ID>\w+)/$', get_related_genes, name="genes"),
     url(r'^api/associated_chemicals/(?P<type>\w+)/(?P<ID>\w+)/$', get_related_chemicals, name="chemicals"),
     url(r'^api/chemical/(?P<ID>\w+)/$', get_single_chem, name="single_chem"),
-
+    url(r'^api/gene/(?P<ID>\w+)/$', get_single_gene, name="single_gene"),
+    url(r'^api/side_effect/(?P<ID>\w+)/$', get_single_side_effect, name="single_side_effect"),
 ]
